8ae25cd4cd62b8f7f9beaf64ac6fb4452c7d1656
Егор Малеев <egor.maleev@hotmail.com>
2021-11-27 00:36:36 +0500

    ad

--- app/src/main/java/com/example/musicStore/SignInActivity.kt
+++ app/src/main/java/com/example/musicStore/SignInActivity.kt
@@ -3,21 +3,48 @@ package com.example.musicStore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var name: EditText
+    lateinit var phone: EditText
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var trypassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-    }
+        name = findViewById(R.id.nameUser)
+        phone = findViewById(R.id.phoneUser)
+        email = findViewById(R.id.emailUser)
+        password = findViewById(R.id.passwordUser)
+        trypassword = findViewById(R.id.passwordTryUser)
 
+
+    }
+    //кнопка вернуться
     fun backUp(view: android.view.View) {
         val intent = Intent (this, SignUpActivity::class.java)
         startActivity(intent)
     }
-
+    //кнопка зарегистрироваться
     fun inSignUp(view: android.view.View) {
-        val intent = Intent (this, SignUpActivity::class.java)
-        startActivity(intent)
+
+        if (phone.text.isNotEmpty() && name.text.isNotEmpty() && email.text.isNotEmpty() && trypassword.text.isNotEmpty() && password.text.isNotEmpty())
+        {
+            if (Patterns.EMAIL_ADDRESS.matcher(email.text).matches())
+            {
+                if (password.text.toString() != trypassword.text.toString())
+                {
+                    AlertDialog.Builder(this@SignInActivity).setMessage("Пароли не совпадают").show()
+                }
+                else startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+            }
+            else AlertDialog.Builder(this@SignInActivity).setMessage("Неверная форма Email").show()
+        }
+        else AlertDialog.Builder(this@SignInActivity).setMessage("Заполните все поля").show()
     }
 
 }
--- app/src/main/java/com/example/musicStore/SignUpActivity.kt
+++ app/src/main/java/com/example/musicStore/SignUpActivity.kt
@@ -3,16 +3,36 @@ package com.example.musicStore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        email = findViewById(R.id.Email)
+        password = findViewById(R.id.Password)
     }
-
+    //кнопка на регистрацию
     fun signInOn(view: android.view.View) {
     val intent = Intent (this, SignInActivity::class.java)
         startActivity(intent)
     }
+    //кнопка войти
+    fun menuIn(view: android.view.View) {
+        if (email.text.isNotEmpty() && password.text.isNotEmpty())
+        {
+            if (Patterns.EMAIL_ADDRESS.matcher(email.text).matches())
+            {
+                startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+            }
+            else AlertDialog.Builder(this@SignUpActivity).setMessage("Неверная форма Email").show()
+        }
+        else AlertDialog.Builder(this@SignUpActivity).setMessage("Заполните все поля").show()
+    }
+
 
 }
